bf8277db616386e5c24770f1c675027767f2bd86
Kristian Røste <github@roste.org>
2023-09-12 11:24:58 +0200

    moved security stuff to wrapper function

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
-<?php header('Content-type: application/json;'); 
+<?php
+/* ────────────────────────────────────────────────────────────────────────── */
+/*                                   index.php                                */
+/* ────────────────────────────────────────────────────────────────────────── */
+/* ──────── Made with ❤️ by darknetzz @ https://github.com/darknetzz ──────── */
+/* ────────────────────────────────────────────────────────────────────────── */
 
+header('Content-type: application/json;'); 
+
+# require_once(ASSETS_FOLDER.'/includes/sqlcon.php');
 require_once('api_settings.php');
 require_once('api_base.php');
 require_once('api_endpoints.php');
@@ -18,38 +26,11 @@ if (!var_assert($args['endpoint'])) {
     die(err("No endpoint provided.", 404));
 }
 
-# We want to allow the API key through GET or headers
-if (var_assert($_SERVER['HTTP_APIKEY'])) {
-    $apikey = $_SERVER['HTTP_APIKEY'];
-} elseif (var_assert($args['apikey'])) {
-    $apikey = $args['apikey'];
-} elseif (var_assert($_GET['apikey'])) {
-    $apikey = $_GET['apikey'];
-} else {
-    die(err("Missing API key", 403));
-}
-
-# If we allow the API key in the header, we might as well allow the endpoint, no?
-
-if (!in_array($args['endpoint'], OPEN_ENDPOINTS)) {
-    $valid_apikey = in_md_array("key", $apikey, API_KEYS);
-    if (!var_assert($apikey) || !$valid_apikey) {
-        die(err("Invalid API key", 403));
-    }
-    $noTimeOut    = API_KEYS[$valid_apikey]["noTimeOut"];
-    $allowedEndpoints = API_KEYS[$valid_apikey]["allowedEndpoints"];
-    if (!in_array("*", $allowedEndpoints) && !in_array($args['endpoint'], $allowedEndpoints)) {
-        die(err("You do not have access to this endpoint.", 403));
-    }
-}
-# !SECTION
-
 $endpoint = "api_".$args['endpoint'];
 
-$functionCall = callFunction($endpoint, $args, $noTimeOut);
+$functionCall = callFunction($endpoint, $args);
 
 $output = $functionCall;
 
 echo $output;
 ?>
-
